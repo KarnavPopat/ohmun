6bb1322428e7b6ff53480408f460d26f59305ea9
Karnav Popat <karnavpopat@gmail.com>
2020-06-03 01:06:46 +0530

    Changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
- AOS.init({
+  AOS.init({
  	duration: 800,
  	easing: 'slide',
  	once: false
@@ -75,7 +75,7 @@ jQuery(document).ready(function($) {
 				$('body').addClass('offcanvas-menu');
 				$this.addClass('active');
 			}
-		}); 
+		}) 
 
 		// click outisde offcanvas
 		$(document).mouseup(function(e) {
@@ -288,7 +288,7 @@ jQuery(document).ready(function($) {
 	var siteCountDown = function() {
 
 		if ( $('#date-countdown').length > 0 ) {
-			$('#date-countdown').countdown('2020/6/20', function(event) {
+			$('#date-countdown').countdown('2020/10/10', function(event) {
 			  var $this = $(this).html(event.strftime(''
 			    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
 			    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
@@ -333,6 +333,6 @@ jQuery(document).ready(function($) {
     $('html,body').animate({
         scrollTop: $(".scrollered").offset().top},
         'slow');
-});
+	});
 
 });
